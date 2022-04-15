0f644da95ba4a8c25fc9d3157a0bc2975367fa47
JiyaDesai1011 <88487835+JiyaDesai1011@users.noreply.github.com>
2022-04-15 17:53:14 +0530

    Delete prc3.py

--- prc3.py
+++
@@ -1,8 +0,0 @@
-#variable for input 
-n = int(input())
-
-#List to store the input
-arr = map(int, input().split())
-
-# Typecasted arr to set to remove repeated the entries, then again to list then sorted the list and printed last second element of the list
-print(sorted(list(set(arr)))[-2])
